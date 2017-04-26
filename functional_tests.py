230ed86624084540431e7fdf08a6718b7c33e825
Milo de Vries <intensmilo@gmail.com>
2017-04-26 12:01:23 +0200

    Expands FT, switches test to "to-do" (woohoo!)

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,30 @@
 from selenium import webdriver
 
 browser = webdriver.Firefox()
+
+# Henk decides to visit this awesome thing he heard about
 browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+# He sees the page title mention to-do lists
+assert 'To-Do' in browser.title
+
+# Toroughly amazed, he immediately want to enter a to-do item
+
+# He types 'choke the chicken' into a text box
+
+# When he hits enter, the page updates and now lists
+# 'choke the chicken' as an item in a to-do list
+
+# Not satisfied, he enters 'wrestle the snake'
+
+# Lo and behold, the page now lists both items!
+
+# Existential questions arise: will this list last?
+# Pondering this Henk notices the url; it's unique
+# Magically some text appeared, informing Henk the url is the key to persistance
+
+# Skeptically he visits this url in pron mode - aha! All is well
+
+# Satisfied, he goes back to sleep
+
+browser.quit()
